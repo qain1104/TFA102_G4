35d62101eea45b0f11b01edcd3e4245639a58368
Vic <vic.liu@webcomm.com.tw>
2021-07-12 20:56:39 +0800

    TFA102Servlet first commit

---
+++ src/main/java/com/git/Tfa102Servlet.java
@@ -0,0 +1,20 @@
+package com.git;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/Tfa102Servlet")
+public class Tfa102Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		res.getWriter().write("Hello World");
+	}
+
+}
